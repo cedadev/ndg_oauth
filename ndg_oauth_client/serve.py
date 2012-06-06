3fd691c25d26ac7759ddcf28edd292732c7b9354
pjkersha <pjkersha@051b1e3e-aa0c-0410-b6c2-bfbade6052be>
2012-06-06 14:38:27 +0000

    Add test files
    git-svn-id: http://proj.badc.rl.ac.uk/svn/ndg-security/trunk/ndg_oauth@8074 051b1e3e-aa0c-0410-b6c2-bfbade6052be

--- ndg_oauth_client/serve.py
+++ ndg_oauth_client/serve.py
@@ -1,6 +1,7 @@
 from paste.script.serve import ServeCommand
 
-ServeCommand("serve").run(["get_url_using_token_app2.ini"])
+ServeCommand("serve").run(["test/cert_req_app.ini"])
+#ServeCommand("serve").run(["get_url_using_token_app2.ini"])
 #ServeCommand("serve").run(["req_cert_app.ini"])
 #ServeCommand("serve").run(["get_url_app_proxy2.ini"])
 #ServeCommand("serve").run(["get_url_app_proxy.ini"])
